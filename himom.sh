d421aee3fc44d52aebcd3cb7d24d334907556f6a
Vin <106352319+iamvin-dev@users.noreply.github.com>
2022-05-27 01:09:29 +0200

    Create himom.sh
    First bash file!

---
+++ himom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Hi Mom!" # is basically just printing text out.
+
+sleep 3 # sleep is basically just a wait. The number after sleep is the amount in seconds you want to wait.
+
+echo "Uh uh"
+
+sleep 3
+
+echo "Oh wow!"
+
+sleep 3
+
+echo "I can't believe that!"
+
+sleep 3
+
+echo "Bye, love you too."
